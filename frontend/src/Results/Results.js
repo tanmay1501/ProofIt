7a7f89c699252b0f8ae9a105ae351dc88ddb6517
Shruthi Srinivas <60729418+shruthey@users.noreply.github.com>
2024-02-18 08:19:46 -0600

    Results testing final

--- frontend/src/Results/Results.js
+++ frontend/src/Results/Results.js
@@ -2,30 +2,26 @@ import React from "react";
 import imageObjects from "../Data/imageObjects";
 import "./Results.scss";
 
-const ResultsPage = ({ items }) => {
+const items = localStorage.getItem("results");
+
+const images = JSON.parse(items).map((item) => {
+  const image = imageObjects.find((obj) => obj.id === item);
+  return (
+    <div className="result-item" key={image.id}>
+      <h3>{image.id}</h3>
+      <div className="image-container">
+        <img src={image.before} alt="before" />
+        <img src={image.after} alt="after" />
+      </div>
+    </div>
+  );
+});
+
+const ResultsPage = () => {
   return (
     <div className="results-page">
-      <h1>Artistic Results Page</h1>
-      {items.map((item) => (
-        <div className="item-container" key={item}>
-          <h3>{item}</h3>
-          {imageObjects[item] &&
-            imageObjects[item].map((picture) => (
-              <div>
-                <img
-                  key={picture.id}
-                  src={picture.before}
-                  alt={`${item} ${picture.id}`}
-                />
-                <img
-                  key={picture.id}
-                  src={picture.after}
-                  alt={`${item} ${picture.id}`}
-                />
-              </div>
-            ))}
-        </div>
-      ))}
+      <h1>Results Page</h1>
+      <div className="results-container">{images}</div>
     </div>
   );
 };
