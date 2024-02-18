e69d045abd9f6230064459492b80e53a0d708791
tanmay1501 <66867695+tanmay1501@users.noreply.github.com>
2024-02-18 04:22:08 -0600

    added summary.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,13 @@ import os
 import cv2
 from ultralytics import YOLO
 from flask import Flask, request, jsonify
+import pathlib
+import textwrap
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
 
 app = Flask(__name__)
 
@@ -54,4 +61,15 @@ def main():
     print(list(objects))
     return list(objects)
 
-    
+def suggestion(objects):
+
+    GOOGLE_API_KEY='AIzaSyD_PgJzgN2ojtIYSRgthWP-foxnYaSVGlY'
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+    model = genai.GenerativeModel('gemini-pro')
+
+
+
+    response = model.generate_content("You are proofing household items to make the room in view more accessible and safer to the following groups of people: young children (newborns and toddlers), the elderly (65+), chronically ill patients, and the physically disabled/handicapped (i.e. those who use assistive devices for mobility such as walkers and wheelchairs). Give 3 suggestions for each category included in the statement on how to proof the item(s) that are detected. Question: " + objects + "are detected in the video. How can we make the room more accessible and safer to the following groups of people: young children (newborns and toddlers), the elderly (65+), chronically ill patients, and the physically disabled/handicapped (i.e. those who use assistive devices for mobility such as walkers and wheelchairs). Give 3 suggestions for each category included in the statement on how to proof the item(s) that are detected.")
+    print(response.text)
+    # return response.text
